2a98c5f90b873d2e50c600e1998ce49e3837c021
cosven <yinshaowen241@gmail.com>
2020-06-03 15:49:04 +0800

    fix: add fuo_kuwo.enc package in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from fuo_kuwo import __version__
 setup(
     name='feeluown_kuwo',
     version=__version__,
-    packages=['fuo_kuwo'],
+    packages=['fuo_kuwo', 'fuo_kuwo.enc'],
     url='https://github.com/BruceZhang1993/feeluown-kuwo',
     license='GPL',
     author='BruceZhang1993',
