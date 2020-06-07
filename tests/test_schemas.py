ec44ad1d4bf0fc14fee20217efb268f578c12b0b
Bruce Zhang <zttt183525594@gmail.com>
2020-06-07 13:14:32 +0800

    tests: add test for some schemas of kuwo

---
+++ tests/test_schemas.py
@@ -0,0 +1,38 @@
+import json
+from urllib.parse import urlparse
+
+from fuo_kuwo.models import _deserialize, KuwoSongModel, KuwoArtistModel, KuwoAlbumModel
+from fuo_kuwo.schemas import KuwoSongSchema, KuwoArtistSchema
+
+
+class TestSchemas:
+    def test_search_song_schema(self):
+        with open('./examples/search.json', 'r') as f:
+            data_songs = json.load(f)
+            for data_song in data_songs.get('data', {}).get('list', []):
+                song = _deserialize(data_song, KuwoSongSchema)
+                assert isinstance(song, KuwoSongModel)
+                assert isinstance(song.identifier, int)
+                assert isinstance(song.title, str)
+                assert isinstance(song.lossless, bool)
+                assert isinstance(song.hasmv, int)
+                assert song.hasmv in [0, 1]
+                assert isinstance(song.duration, int)
+                if song.artists and len(song.artists) > 0:
+                    assert isinstance(song.artists[0], KuwoArtistModel)
+                if song.album:
+                    assert isinstance(song.album, KuwoAlbumModel)
+
+    def test_search_artist_schema(self):
+        with open('./examples/search_artist.json', 'r') as f:
+            data_artists = json.load(f)
+            for data_artist in data_artists.get('data', {}).get('artistList', []):
+                artist = _deserialize(data_artist, KuwoArtistSchema)
+                assert isinstance(artist.identifier, int)
+                assert isinstance(artist.name, str)
+                assert isinstance(artist.cover, str)
+                uri = urlparse(artist.cover)
+                assert uri.hostname == 'star.kuwo.cn'
+                uri.path.endswith('.jpg')
+                assert isinstance(artist.desc, str)
+                assert isinstance(artist.info, str)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import json
 
-from fuo_kuwo.utils import parse_lrc_time, parse_lyrics
+from fuo_kuwo.utils import parse_lrc_time, parse_lyrics, digest_encrypt
 
 
 class TestUtils:
@@ -14,3 +14,9 @@ class TestUtils:
             data = json.load(f)
             content = parse_lyrics(data.get('data').get('lrclist'))
             assert isinstance(content, str)
+
+    def test_digest_encrypt(self):
+        s = 'This is a test string'
+        res = digest_encrypt(s)
+        assert isinstance(s, str)
+        assert res == 'c639efc1e98762233743a75e7798dd9c'
